9116e49d27ae702fe8a2dda0c3959826c2dc909b
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2024-01-18 20:39:44 +0200

    add city function

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyCity from './components/myCity';
+
+function App() {
+  return (
+    <div className="App">
+      <MyCity/>      
+    </div>
+    
+  );
+}
+
+export default App;
